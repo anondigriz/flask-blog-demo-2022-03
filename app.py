7e6decbe3d41de5f67eccc7ec5d9804ab4d29ba1
Dmitry Aladin <anon.digriz@gmail.com>
2022-03-24 19:35:40 +0300

    Создание поста

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 def get_db_connection():
     conn = sqlite3.connect(os.getenv('FLASK_DB'))
     conn.row_factory = sqlite3.Row
     return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 @app.route("/")
 def index():
@@ -16,6 +30,31 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
