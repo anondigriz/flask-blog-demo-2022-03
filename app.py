3c825c93e1d579503c6eb9cfcaf22d123403a41d
Dmitry Aladin <anon.digriz@gmail.com>
2022-03-24 18:55:00 +0300

    Отображение постов на главной странице

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-import imp
+import sqlite3
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect(os.getenv('FLASK_DB'))
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 @app.route("/about")
 def about():
